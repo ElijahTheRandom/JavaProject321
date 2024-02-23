6a44ebb70e3e294763f1078eb680ad3778d93bfb
Ethan Jones <etn.jones@gmail.com>
2024-02-23 09:59:00 -0600

    check-in of a skeleton.

--- src/tagifyme/model/Database.java
+++ src/tagifyme/model/Database.java
@@ -2,23 +2,57 @@ package tagifyme.model;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 // TODO: Abstract this later with inheritance, etc.
 public class Database {
 
-  // TODO: `List` seems to be the minimum amount of specificity
-  // that we could use, but there's benefits to using an ordered list.
-  // Does the stdlib have anything like this in-built?
+  // TODO: These are lists at this point, but there's cases where we don't want
+  // to add duplicate tags within the database, etc. There's the case where
+  // we potentially want these ordered to allow for faster searching.
+  // Select some type, and then figure it out later.
   private List<Data> data_list;
-  private List<Tag>  tag_list;
+  private List<Tag> tag_list;
   private List<Relationship> relationship_list;
 
   public Database() {
+    // See the above TODO.
     this.data_list = new ArrayList<Data>();
     this.tag_list = new ArrayList<Tag>();
     this.relationship_list = new ArrayList<Relationship>();
   }
+  
+  public void addTag(Tag t) {
+      // TODO: Add a tag to the databse.
+  }
+  
+  public void deleteTag(Tag t) {
+      // TODO: Implementation to delete a tag from the database.
+      // NOTE: this has to delete the accompanying relationships aswell.
+  }
+  
+  public void addData(Data dt) {
+      // TODO: Add data to the database.
+  }
+  
+  // TODO: Does this take a Data (an object reference), or does it take
+  // an index into the data type that stores the relationship?
+  public void deleteData(Data dt) {
+      // TODO: Remove specific data from the database; NOTE, this should
+      // destroy the accompanying relationships within the database aswell.
+  }
+  
+  public void addRelationship(Relationship rel) {
+      // TODO: Implementation to add the specific relationship to the
+      // database.
+  }
+  
+  // TODO: Does this take a Relationship (an object reference), or does it
+  // take an index into the data type that stores the relationship?
+  public void deleteRelationship(Relationship rel) {
+      // TODO: Implementation to remove the specific relationship from the
+      // database.
+  }
+ 
 
   public static Database loadDatabase() throws Exception {
     // TODO: Load a database from a file.
@@ -27,20 +61,5 @@ public class Database {
 
   public void dumpDatabase() {
     // TODO: Write the database to disk.
-  }
-
-  // TODO: At some point, when feeding to a view, we'll need to iterate
-  // over the entirety of the data.
-
-  public Iterator<Data> getData() {
-    return this.data_list.iterator();
-  }
-
-  public Iterator<Tag> getTag() {
-    return this.tag_list.iterator();
-  }
-
-  public Iterator<Relationship> getRelationship() {
-    return this.relationship_list.iterator();
   }
 }
