db91cfd1b20774cf38349e7aea282893cae87bfb
Ethan Jones <etn.jones@gmail.com>
2024-02-24 02:04:57 -0600

    Equivalence/hashCode for tags.
    The identifier is what truly matters for equivalence for tags; NOTE:
    this changes if we want to do some sorting on an ordinal value inside
    the tag, or something to that degree.

--- src/tagifyme/model/Tag.java
+++ src/tagifyme/model/Tag.java
@@ -26,4 +26,26 @@ public class Tag implements Comparable<Tag> {
   public int compareTo(Tag other) {
     return this.getName().compareTo(other.getName());
   }
+
+  // NOTE: Both of these Overrides are needed to have the Set functionality we
+  // require; a Tag should be differentiated based upon its string indentifier,
+  // not its object. See the above compareTo.
+  
+  /**
+   * Return the hashCode for this object.
+   */
+  @Override
+  public int hashCode() {
+    return this.getName().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || obj.getClass() != this.getClass()) {
+      return false;
+    }
+
+    final Tag other = (Tag) obj;
+    return this.getName().equals(other.getName());
+  }
 }
