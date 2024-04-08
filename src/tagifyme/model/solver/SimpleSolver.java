ad5d480af18496bc3290e5d7cacd1bbcb3ce5c6b
Ethan Jones <etn.jones@gmail.com>
2024-04-07 20:11:35 -0500

    bugfix.

--- src/tagifyme/model/solver/SimpleSolver.java
+++ src/tagifyme/model/solver/SimpleSolver.java
@@ -1,17 +1,17 @@
 package tagifyme.model.solver;
 
-import tagify.model.Relationship;
+import tagifyme.model.Relationship;
+import tagifyme.model.Tag;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
-import java.util.Iterable;
 
 public class SimpleSolver implements Solver {
-  public SimpleSolver {}
 
   public List<Relationship> filter(Set<Tag> constraints, Iterable<Relationship> rel_iter) {
     ArrayList<Relationship> filtered = new ArrayList();
     for (Relationship r : rel_iter) {
-      if (constraint.contains(r.getTag())) {
+      if (constraints.contains(r.getTag())) {
         filtered.add(r);
       }
     }
--- src/tagifyme/model/solver/Solver.java
+++ src/tagifyme/model/solver/Solver.java
@@ -1,9 +1,9 @@
 package tagifyme.model.solver;
 
 import tagifyme.model.Relationship;
+import tagifyme.model.Tag;
 import java.util.List;
 import java.util.Set;
-import java.util.Iterable;
 
 public interface Solver {
   public List<Relationship> filter(Set<Tag> constraints, Iterable<Relationship> rel_iter);
